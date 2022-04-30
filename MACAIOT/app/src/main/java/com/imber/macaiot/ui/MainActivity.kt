9d2b76e7dec1c36527f93840afd11a25847bcc81
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2022-05-01 02:35:26 +0300

    Added Graph view and data reads succesfully

--- MACAIOT/app/src/main/java/com/imber/macaiot/ui/MainActivity.kt
+++ MACAIOT/app/src/main/java/com/imber/macaiot/ui/MainActivity.kt
@@ -2,7 +2,9 @@ package com.imber.macaiot.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.imber.macaiot.R
@@ -10,9 +12,12 @@ import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
 import java.util.*
+import kotlin.reflect.typeOf
 
 class MainActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +25,12 @@ class MainActivity : AppCompatActivity() {
 
         val calendar = Calendar.getInstance()
 
-        var month : Int = calendar.get(Calendar.DAY_OF_MONTH)
+        var day : Int = calendar.get(Calendar.DAY_OF_MONTH)
+        var month : Int = calendar.get(Calendar.MONTH) +1 //The Months are numbered from 0 (January) to 11 (December).
         var year : Int = calendar.get(Calendar.YEAR)
 
-        print("KISSSAA")
-        println(month)
-        println(year)
-        readData(month,year)
 
+        readData(day,month,year)
 
 
 
@@ -36,8 +39,24 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    private fun readData(monthD : Int, year : Int){
-        database = FirebaseDatabase.getInstance().getReference(year.toString()).child(monthD.toString())
+    private fun readData(day : Int,monthD : Int, year : Int){
+        database = FirebaseDatabase.getInstance().getReference(year.toString()).child(monthD.toString()).child(day.toString())
+        database.get().addOnSuccessListener {
+            if (it.exists()){
+                println(it.childrenCount)
+                it.children.forEach { x -> println(x.value) }
+            }else{
+                println("why man")
+                Toast.makeText(this,"Not Found",Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener{
+            println("CANT REAAAD")
+            Toast.makeText(this,"Could Not read Data",Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
     }
 
 
