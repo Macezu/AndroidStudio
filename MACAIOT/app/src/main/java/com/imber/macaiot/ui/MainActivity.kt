68d1ec4dbe1be3d82f3ea4a0437a1e252c56d05c
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2022-04-30 21:01:36 +0300

    Initial Commit

---
+++ MACAIOT/app/src/main/java/com/imber/macaiot/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.imber.macaiot.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.imber.macaiot.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
