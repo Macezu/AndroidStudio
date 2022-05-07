e962bb91e54200147991cfb74911279ecde39506
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2022-05-07 23:41:07 +0300

    Updated notification function

--- MACAIOT/functions/src/index.ts
+++ MACAIOT/functions/src/index.ts
@@ -1,8 +1,17 @@
 /* eslint-disable max-len */
 // The Cloud Functions for Firebase SDK create Cloud Functions set up triggers.
 import * as functions from "firebase-functions";
-import {CloudFunction} from "firebase-functions";
-import {QueryDocumentSnapshot} from "firebase-functions/v1/firestore";
+
+import * as admin from "firebase-admin";
+import {MessagingDevicesResponse} from "firebase-admin/lib/messaging/messaging-api";
+
+admin.initializeApp();
+
+const currentTime: Date = new Date();
+const year: string = currentTime.getFullYear().toString();
+const month: string = (currentTime.getMonth() + 1).toString();
+const day: string = currentTime.getDate().toString();
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -13,25 +22,51 @@ export const helloWorld = functions.https.onRequest((_request, response) => {
 });
 
 
-export const logLatest = (): CloudFunction<QueryDocumentSnapshot> => {
-  const currentTime: Date = new Date();
-  const year: string = currentTime.getFullYear().toString();
-  const month: string = (currentTime.getMonth() + 1).toString();
-  const day: string = currentTime.getDate().toString();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const getLatestMetric = functions.firestore.document(`"/${year}/${month}/${day}"`)
-      .onCreate((snap, context) => {
-      // Grab the current value of what was written to Firestore.
-        const original = snap.data().original;
+// Sends a notifications to all users when a new message is posted.
+exports.sendNotifications = functions.firestore.document(`"/${year}/${month}/${day}"`).onCreate(
+    async (snapshot) => {
+      // Notification details.
+      const text = snapshot.data().text;
+      functions.logger.log(text);
+      const payload = {
+        notification: {
+          title: `${snapshot.data().name} posted ${text ? "a message" : "an image"}`,
+          body: text ? (text.length <= 100 ? text : text.substring(0, 97) + "...") : "",
+          click_action: `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com`,
+        },
+      };
 
-        // Access the parameter `{documentId}` with `context.params`
-        functions.logger.log("Uppercasing", context.params.documentId, original);
-
-
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to Firestore.
-        // Setting an 'uppercase' field in Firestore document returns a Promise.
-        return snap.ref.set({original}, {merge: true});
+      // Get the list of device tokens.
+      const allTokens = await admin.firestore().collection("fcmTokens").get();
+      const tokens: string | string[] = [];
+      allTokens.forEach((tokenDoc) => {
+        tokens.push(tokenDoc.id);
       });
-  return getLatestMetric;
-};
+
+      if (tokens.length > 0) {
+        // Send notifications to all tokens.
+        const response = await admin.messaging().sendToDevice(tokens, payload);
+        await cleanupTokens(response, tokens);
+        functions.logger.log("Notifications have been sent and tokens cleaned up.");
+      }
+    });
+
+// Cleans up the tokens that are no longer valid.
+// eslint-disable-next-line require-jsdoc
+function cleanupTokens(response: MessagingDevicesResponse, tokens: string[]) {
+  // For each notification we check if there was an error.
+  const tokensDelete: unknown[] = [];
+  response.results.forEach((result, index) => {
+    const error = result.error;
+    if (error) {
+      functions.logger.error("Failure sending notification to", tokens[index], error);
+      // Cleanup the tokens that are not registered anymore.
+      if (error.code === "messaging/invalid-registration-token" ||
+          error.code === "messaging/registration-token-not-registered") {
+        const deleteTask = admin.firestore().collection("fcmTokens").doc(tokens[index]).delete();
+        tokensDelete.push(deleteTask);
+      }
+    }
+  });
+  return Promise.all(tokensDelete);
+}
